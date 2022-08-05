39acf353c4b6b04532d4656b0193ec9bff402dfa
Rado <R.A.Todorov@student.tudelft.nl>
2022-08-05 15:34:17 +0200

    Created a class.

---
+++ consumer.py
@@ -0,0 +1,13 @@
+class Consumer:
+
+    def __init__(self):
+        self.count = 0
+
+    def increment(self):
+        self.count += 1
+
+    def decrement(self):
+        self.count -= 1
+
+    def get_count(self):
+        return self.count
