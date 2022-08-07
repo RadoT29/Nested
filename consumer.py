4b92735539890499401737e0046d5a2ae87bcce5
Rado <R.A.Todorov@student.tudelft.nl>
2022-08-07 22:38:32 +0200

    Change steps

--- consumer.py
+++ consumer.py
@@ -4,10 +4,10 @@ class Consumer:
         self.count = 0
 
     def increment(self):
-        self.count += 2
+        self.count += 1
 
     def decrement(self):
-        self.count -= 2
+        self.count -= 1
 
     def get_count(self):
         return self.count
